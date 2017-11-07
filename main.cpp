24ec9d70749c21e9efbe43841a10176157e56c20
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2017-11-07 21:37:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,60 +1,63 @@
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstring>
 
 using namespace std;
 
+double add(double a, double b)
+{ return a + b; }
+double sub(double a, double b)
+{ return a - b; }
+double mul(double a, double b)
+{ return a * b; }
+double div(double a, double b)
+{ return a / b; }
+
 int main(int argc, char* argv[])
 {
-    int elements = argc - 1;
+    int elements = strlen(argv[1]);
     int razmch = (elements + 1) / 2;
-    int razmzn = (elements + 1) / 2 - 1;
+    int razmzn = razmch - 1;
     char chisla[razmch];
     char znaks[razmzn];
-    for (int i = 0; i < razmch - 1; i++)
+    for (int i = 0; i < razmzn; i++)
     {
-        znaks[i] = argv[2 * (i + 1)][0];
+        znaks[i] = argv[1][2 * i + 1];
     }
 
     for (int j = 0; j < razmch; j++)
     {
-        chisla[j] = argv[2 * j + 1][0];
+        chisla[j] = argv[1][2 * j];
     }
-    for (int x = 0; x < razmch - 1; x++)
-    {
-        if (znaks[x] == '*')
-            rez1[x] = atoi(chisla[x - 1]) * atoi(chisla[x + 1]);
-        if (znaks[x] == '/')
-            rez1[x] = atoi(chisla[x - 1]) / atoi(chisla[x + 1]);
+
+    //Массивы чисел и знаков заполнены
+
+    for (int x = 0; x < razmzn; x++) {
+    if (znaks[x] == '*') {
+      chisla[x] = mul((double)(chisla[x]), (double)(chisla[x + 1]));
+      razmch = razmch - 1;
     }
-    for (int y = 0; y < razmch - 1; y++)
-    {
-        if (znaks[y] == '+')
-        {
-            if (znaks[y + 1] == '*')
-                rez = chisla[y] + rez[x];
-            if (znaks[y - 1] == '/')
-                rez = chisla[y] + rez[x];
-            if (znaks[y - 1] == '*')
-                rez = chisla[y] + rez[x];
-            if (znaks[y + 1] == '/')
-                rez = chisla[y] + rez[x];
-            else
-                rez = chisla[y - 1] + chisla[y + 1]
-        }
-        if (znaks[y] == '-')
-        {
-            if (znaks[y + 1] == '*')
-                rez = chisla[y] - rez[x];
-            if (znaks[y - 1] == '/')
-                rez = rez[x] - chisla[y];
-            if (znaks[y - 1] == '*')
-                rez = rez[x] - chisla[y];
-            if (znaks[y + 1] == '/')
-                rez = chisla[y] - rez[x];
-            else
-                rez = chisla[y - 1] - chisla[y + 1]
-        }
-        cout << rez;
-        return 0;
+}
+    for (int x = 0; x < razmzn; x++) {
+    if (znaks[x] == '/') {
+      chisla[x] = div((double)(chisla[x]), (double)(chisla[x + 1]));
+      razmch = razmch - 1;
     }
+}
+     for (int x = 0; x < razmzn; x++) {
+     if (znaks[x] == '+') {
+      chisla[x] = add((double)(chisla[x]), (double)(chisla[x + 1]));
+      razmch = razmch - 1;
+    }
+}
+     for (int x = 0; x < razmzn; x++) {
+     if (znaks[x] == '-') {
+      chisla[x] = sub((double)(chisla[x]), (double)(chisla[x + 1]));
+      razmch = razmch - 1;
+    }
+}
+
+    cout << chisla[0];
+}
+
