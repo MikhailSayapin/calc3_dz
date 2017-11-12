ad1054a6a003155318cff95e7534e757c7a468d6
MikhailSayapin <32384891+MikhailSayapin@users.noreply.github.com>
2017-11-13 01:25:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,29 +5,21 @@
 
 using namespace std;
 
-double add(double a, double b)
+void shift(int i, int razmzn, double chisla[], char znaks[])
 {
-    return a + b;
+    for (int j = i + 1; j < razmzn; j++) {
+    chisla[j] = chisla[j + 1];
+  };
+  for (int j = i; j < razmzn-1; j++) {
+    znaks[j] = znaks[j + 1];
+  };
 }
-double sub(double a, double b)
-{
-    return a - b;
-}
-double mul(double a, double b)
-{
-    return a * b;
-}
-double div(double a, double b)
-{
-    return a / b;
-}
-
 int main(int argc, char* argv[])
 {
     int elements = strlen(argv[1]);
     int razmch = (elements + 1) / 2;
     int razmzn = razmch - 1;
-    char chisla[razmch];
+    double chisla[razmch];
     char znaks[razmzn];
     for (int i = 0; i < razmzn; i++)
     {
@@ -36,43 +28,49 @@ int main(int argc, char* argv[])
 
     for (int j = 0; j < razmch; j++)
     {
-        chisla[j] = argv[1][2 * j];
+        chisla[j] = (double)(argv[1][2 * j]);
     }
 
     //Массивы чисел и знаков заполнены
 
-    for (int x = 0; x < razmzn; x++)
+    for (int i = 0; i < razmzn; i++)
     {
-        if (znaks[x] == '*')
+        if (znaks[i] == '*')
         {
-            chisla[x] = mul((double)(chisla[x]), (double)(chisla[x + 1]));
-            razmch = razmch - 1;
+            chisla[i] = (double)(chisla[i]) * (double)(chisla[i + 1]);
+            shift(i, razmzn, chisla, znaks);
+            razmzn = razmzn - 1;
         }
     }
-    for (int x = 0; x < razmzn; x++)
+    for (int i = 0; i < razmzn; i++)
     {
-        if (znaks[x] == '/')
+        if (znaks[i] == '/')
         {
-            chisla[x] = div((double)(chisla[x]), (double)(chisla[x + 1]));
-            razmch = razmch - 1;
+            chisla[i] = (double)(chisla[i]) / (double)(chisla[i + 1]);
+            shift(i, razmzn, chisla, znaks);
+            razmzn = razmzn - 1;
         }
     }
-    for (int x = 0; x < razmzn; x++)
+    for (int i = 0; i < razmzn; i++)
     {
-        if (znaks[x] == '+')
+        if (znaks[i] == '+')
         {
-            chisla[x] = add((double)(chisla[x]), (double)(chisla[x + 1]));
-            razmch = razmch - 1;
+            chisla[i] = (double)(chisla[i]) + (double)(chisla[i + 1]);
+            shift(i, razmzn, chisla, znaks);
+            razmzn = razmzn - 1;
         }
     }
-    for (int x = 0; x < razmzn; x++)
+    for (int i = 0; i < razmzn; i++)
     {
-        if (znaks[x] == '-')
+        if (znaks[i] == '-')
         {
-            chisla[x] = sub((double)(chisla[x]), (double)(chisla[x + 1]));
-            razmch = razmch - 1;
+            chisla[i] = (double)(chisla[i]) - (double)(chisla[i + 1]);
+            shift(i, razmzn, chisla, znaks);
+            razmzn = razmzn - 1;
         }
     }
 
     cout << chisla[0];
+    return 0;
 }
+
